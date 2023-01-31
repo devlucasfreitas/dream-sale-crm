715f64f307ce5f7f27cf0fa5ceb30a2439498f98
Lucas Ferreira Freitas <lucasf.freitas@outlook.com>
2023-01-31 13:54:58 -0300

    refactor: removendo paginas login e cadastro

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./components/pages/dashboard/Dashboard";
-import UserCadastro from "./components/pages//login/UserCadastro";
 import Clientes from "./components/pages/clientes/Clientes";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/cadastro" element={<UserCadastro />} />
         <Route path="/clientes" element={<Clientes />} />
       </Routes>
     </BrowserRouter>
