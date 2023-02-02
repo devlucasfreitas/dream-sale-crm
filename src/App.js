69f3878aeeb8bb51feeeba582a8fbc94931dd69a
Lucas Ferreira Freitas <lucasf.freitas@outlook.com>
2023-02-02 18:59:24 -0300

    add express

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/pages/dashboard/Dashboard";
 import Clientes from "./components/pages/clientes/Clientes";
 
+const { join } = require("path");
+const express = require("express");
+const app = express();
+
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "build", "index.html"));
+});
+
+app.listen(3000);
+console.log("localhost:3000");
+
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
